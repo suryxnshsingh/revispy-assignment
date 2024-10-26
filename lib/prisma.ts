7fd5ffa1570356ecbb8132d0fd41e707e70736f9
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2024-10-26 22:57:05 +0530

    f

---
+++ lib/prisma.ts
@@ -0,0 +1,8 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as typeof global & { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
